5b19e561503c771b174c2daf29ca4a89c023d4bc
Manish Manwani <manishmanwani73@gmail.com>
2023-07-04 20:32:09 +0930

    feat(billboard image upload): hooked up cloudinary to setup billboard image uploads

---
+++ components/ui/image-upload.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ImagePlus, Trash } from "lucide-react";
+import { CldUploadWidget } from "next-cloudinary";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+interface Props {
+  disabled?: boolean;
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+  value: string[];
+}
+
+export default function ImageUpload({
+  disabled,
+  onChange,
+  onRemove,
+  value,
+}: Props) {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const onUpload = (result: any) => {
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) return null;
+
+  return (
+    <div>
+      <div className="mb-4 flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+            key={url}
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type="button"
+                onClick={() => onRemove(url)}
+                variant="destructive"
+                size="icon"
+              >
+                <Trash className="w-4 h-4" />
+              </Button>
+            </div>
+            <Image fill className="object-cover" alt="Image" src={url} />
+          </div>
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="tlpleegm">
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+          return (
+            <Button
+              type="button"
+              onClick={onClick}
+              variant="secondary"
+              disabled={disabled}
+            >
+              <ImagePlus className="w-4 h-4 mr-2" />
+              Upload an image
+            </Button>
+          );
+        }}
+      </CldUploadWidget>
+    </div>
+  );
+}
