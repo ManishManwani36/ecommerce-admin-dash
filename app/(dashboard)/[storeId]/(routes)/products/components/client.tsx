20a6771241a0f6f3343e43da383022cbe7b4030c
Manish Manwani <manishmanwani73@gmail.com>
2023-07-06 21:07:41 +0930

    feat(product entity): added product entity

---
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { ApiList } from "@/components/ui/api-list";
+import { Button } from "@/components/ui/button";
+import { DataTable } from "@/components/ui/data-table";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Plus } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { ProductColumn, columns } from "./columns";
+
+interface Props {
+  data: ProductColumn[];
+}
+
+export default function ProductClient({ data }: Props) {
+  const router = useRouter();
+  const params = useParams();
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title={`Products (${data.length})`}
+          description="Manage your store products."
+        />
+        <Button onClick={() => router.push(`/${params.storeId}/products/new`)}>
+          <Plus className="mr-2 h-4 w-4" />
+          Add New
+        </Button>
+      </div>
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+      <Heading title="API" description="API calls for products." />
+      <Separator />
+      <ApiList entityName="products" entityIdName="productId" />
+    </>
+  );
+}
